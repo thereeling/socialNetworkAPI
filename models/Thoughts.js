fce0e3ef3a9ea028c46214eb846f48c6de9ef256
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-11-20 16:47:05 -0500

    thoughts model

---
+++ models/Thoughts.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateformat');
+
+const ThoughtsSchema = new Schema ({
+    thoughtText: {
+        type: String,
+        required: 'A thought is required',
+        min: 1,
+        max: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: 'A user is required'
+    },
+    reactions: [ReactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+}
+);
+
+ThoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+module.exports = Thoughts;
