fd7c7be73b3c70eacafafcda265e69eabe28ebba
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-11-20 16:36:56 -0500

    User schema

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema ({
+    username: {
+        type: String,
+        unique: true,
+        required: 'Username is Required',
+        trim: true
+    },
+    email: {
+        type: String,
+        required: 'Email is required',
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    thoughts: {
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts'
+    },
+    friends: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+}
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
